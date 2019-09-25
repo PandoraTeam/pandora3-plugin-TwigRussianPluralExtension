2dc5175429b12ea9515a38f4f951fd6e7a1188ee
Petr Gostin <U.238@rambler.ru>
2019-09-25 19:31:43 +0300

    initial commit

---
+++ TwigRussianPluralExtension.php
@@ -0,0 +1,28 @@
+<?php
+namespace Pandora3\Plugins\TwigRussianPluralExtension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TwigRussianPluralExtension extends AbstractExtension {
+
+	public function getFilters() {
+		return [
+			new TwigFilter('plural', function($values, $count) {
+				$mod10 = $count % 10;
+				$mod100 = $count % 100;
+				if (
+					$mod10 === 0 || $mod10 >= 5 ||
+					($mod100 >= 10 && $mod100 < 20)
+				) {
+					$index = 2;
+				} else {
+					$index = ($mod10 === 1) ? 0 : 1;
+				}
+				return sprintf($values[$index] ?? '', $count);
+			})
+		];
+	}
+
+}
+
